ba2377ce6cf13417492810d01fa7f05a68789661
MitchellSlav <s3723026@student.rmit.edu.au>
2020-08-17 05:33:59 +1000

    Spring framework setup

---
+++ BackEnd/src/main/java/com/septproject/wed_16_30_2/Wed16302Application.java
@@ -0,0 +1,13 @@
+package com.septproject.wed_16_30_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wed16302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wed16302Application.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/septproject/wed_16_30_2/Wed16302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.septproject.wed_16_30_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wed16302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
